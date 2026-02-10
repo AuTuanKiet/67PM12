8d839265a60b376dde2fc2d3bac2ff59f8ee45d6
AuTuanKiet <aukiet12022004@gmail.com>
2026-02-10 17:11:40 +0700

    Chỉnh sửa ProductController ngày 10/2/2026

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -38,7 +38,7 @@ class ProductController extends Controller implements HasMiddleware
         ]);
 
         Product::create($data);
-        return redirect()->route('product.index')->with('success', 'Thêm sản phẩm mới thành công');
+        return redirect()->route('product.index')->with('success', 'Thêm mới sản phẩm thành công!');
     }
 
     public function edit(string $id) {
@@ -56,14 +56,14 @@ class ProductController extends Controller implements HasMiddleware
     ]);
 
         $product->update($data);
-        return redirect()->route('product.index')->with('success', 'Cập nhật sản phẩm thành công');
+        return redirect()->route('product.index')->with('success', 'Cập nhật sản phẩm thành công!');
     }
 
     public function destroy(string $id) {
         $product = Product::findOrFail($id);
         $product->delete();
 
-        return redirect()->route('product.index')->with('success', 'Xóa sản phẩm thành công');
+        return redirect()->route('product.index')->with('success', 'Xóa sản phẩm thành công!');
     }
 
     public function login() {
